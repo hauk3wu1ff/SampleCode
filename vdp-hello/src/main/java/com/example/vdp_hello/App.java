aba357aab17116fef0ce9f72ddc1912d7ffb612f
HaWu1 <wulffhauke@eircom.net>
2018-05-28 13:46:48 +0100

    Completed helloworld with client-cert, auth and proxy.

--- vdp-hello/src/main/java/com/example/vdp_hello/App.java
+++ vdp-hello/src/main/java/com/example/vdp_hello/App.java
@@ -1,5 +1,6 @@
 package com.example.vdp_hello;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -7,17 +8,27 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 
 /**
@@ -28,28 +39,54 @@ public class App {
 	public static void main(String[] args) throws KeyManagementException, UnrecoverableKeyException,
 			NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException {
 		System.out.println("Hello World!");
+		if (args.length != 5) {
+			System.out.println(
+					"Please call with parameters: <USER> <PASSWORD> <KEY_STORE_PATH> <KEY_STORE_PASSWORD> <PRIVATE_KEY_PASSWORD>");
+			System.exit(0);
+		}
 
-		/*
-		 * String KEY_STORE_PATH = args[0]; String KEY_STORE_PASSWORD = args[1]; String
-		 * PRIVATE_KEY_PASSWORD = args[2];
-		 * 
-		 * // Load client certificate into key store SSLContext sslcontext =
-		 * SSLContexts.custom().loadKeyMaterial(new File(KEY_STORE_PATH),
-		 * KEY_STORE_PASSWORD.toCharArray(),
-		 * PRIVATE_KEY_PASSWORD.toCharArray()).build(); // Allow TLSv1.2 protocol only
-		 * SSLConnectionSocketFactory sslSocketFactory = new
-		 * SSLConnectionSocketFactory(sslcontext, new String[] { "TLSv1.2" }, null,
-		 * SSLConnectionSocketFactory.getDefaultHostnameVerifier()); CloseableHttpClient
-		 * httpClient =
-		 * HttpClients.custom().setSSLSocketFactory(sslSocketFactory).build();
-		 */
+		String USER = args[0];
+		String PASSWORD = args[1];
+		String KEY_STORE_PATH = args[2];
+		String KEY_STORE_PASSWORD = args[3];
+		String PRIVATE_KEY_PASSWORD = args[4];
 
+		// Load client certificate into key store
+		SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(new File(KEY_STORE_PATH),
+				KEY_STORE_PASSWORD.toCharArray(), PRIVATE_KEY_PASSWORD.toCharArray()).build();
+
+		// Allow TLSv1.2 protocol only
+		SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslcontext,
+				new String[] { "TLSv1.2" }, null, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+		// CloseableHttpClient httpClient =
+		// HttpClients.custom().setSSLSocketFactory(sslSocketFactory).build();
+
+		HttpHost target = new HttpHost("sandbox.api.visa.com", 443, "https");
 		CredentialsProvider credsProvider = new BasicCredentialsProvider();
-		credsProvider.setCredentials(new AuthScope("httpbin.org", 80),
-				new UsernamePasswordCredentials("user", "passwd"));
-		CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+		credsProvider.setCredentials(new AuthScope(target.getHostName(), target.getPort()),
+				new UsernamePasswordCredentials(USER, PASSWORD));
+		CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider)
+				.setSSLSocketFactory(sslSocketFactory).build();
 		try {
-			HttpGet httpget = new HttpGet("http://httpbin.org/basic-auth/user/passwd");
+
+			// Preemptive Authentication
+			// Create AuthCache instance
+			AuthCache authCache = new BasicAuthCache();
+			// Generate BASIC scheme object and add it to the local
+			// auth cache
+			BasicScheme basicAuth = new BasicScheme();
+			authCache.put(target, basicAuth);
+
+			// Add AuthCache to the execution context
+			HttpClientContext localContext = HttpClientContext.create();
+			localContext.setAuthCache(authCache);
+
+			// Proxy setup
+			HttpHost proxy = new HttpHost("127.0.0.1", 3128, "http");
+			RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+
+			HttpGet httpget = new HttpGet("https://sandbox.api.visa.com/vdp/helloworld");
+			httpget.setConfig(config);
 
 			// Create a custom response handler
 			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
@@ -70,7 +107,7 @@ public class App {
 			};
 
 			System.out.println("Executing request " + httpget.getRequestLine());
-			System.out.println(httpclient.execute(httpget, responseHandler));
+			System.out.println(httpclient.execute(target, httpget, responseHandler, localContext));
 
 		} finally {
 			httpclient.close();
